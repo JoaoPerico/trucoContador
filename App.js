a3e82f9d5df748bd9e69d8b760c20fe5b40deea9
Joao Périco <joaovitorperico@gmail.com>
2026-03-20 16:00:58 -0300

    Projeto inicial truco contador

--- App.js
+++ App.js
@@ -1,10 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [contador1, setContador1] = useState(0);
+  const [contador2, setContador2] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{ backgroundColor: "#a93a3a", padding: 10, borderRadius: 5 }}
+      >
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+          Contador de Truco
+        </Text>
+      </View>
+
+      <View style={{ flexDirection: "row", gap: 20 }}>
+        <View style={{ alignItems: "center", gap: 10 }}>
+          <Text>Nós</Text>
+          <Text>{contador1}</Text>
+
+          <TouchableOpacity
+            style={styles.botaoMais}
+            onPress={() => setContador1(contador1 + 1)}
+          >
+            <Text>Aumentar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.botaoMenos}
+            onPress={() => setContador1(contador1 - 1)}
+          >
+            <Text>Diminuir</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ alignItems: "center", gap: 10 }}>
+          <Text>Eles</Text>
+          <Text>{contador2}</Text>
+
+          <TouchableOpacity
+            style={styles.botaoMais}
+            onPress={() => setContador2(contador2 + 1)}
+          >
+            <Text>Aumentar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.botaoMenos}
+            onPress={() => setContador2(contador2 - 1)}
+          >
+            <Text>Diminuir</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +63,32 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    gap: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#5d5959",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  botaoMais: {
+    backgroundColor: "#4CAF50",
+    padding: 15,
+    borderRadius: 10,
+    width: 100,
+    alignItems: "center",
+  },
+
+  botaoMenos: {
+    backgroundColor: "#F44336",
+    padding: 15,
+    borderRadius: 10,
+    width: 100,
+    alignItems: "center",
+  },
+
+  textoBotao: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
